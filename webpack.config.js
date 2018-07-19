246de88b74f94d4143c94bc894cc403b7d6affe2
AfterWind <spmushroom@hotmail.com>
2018-07-19 09:54:08 +0800

    环境准备

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    app: [
+      './test/index.ts'
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.ts'],
+    modules: [
+      './src',
+      './node_modules',
+    ],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: [
+          'ts-loader',
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './test/index.html',
+      inject: 'body',
+    }),
+  ],
+  devServer: {
+    port: '12347',
+    contentBase: './build',
+    watchContentBase: true,
+    stats: { colors: true },
+  },
+};
